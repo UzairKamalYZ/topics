7299d46a34d190fe56fa3722c731966ca2daab1e
ukamal <ukamal@isabelgroup.eu>
2022-07-07 17:27:39 +0200

    feat(custom annotation): implementing custom processor.

--- customannotation/src/main/java/be/techfix/annotations/custom/History.java
+++ customannotation/src/main/java/be/techfix/annotations/custom/History.java
@@ -6,4 +6,13 @@ public class History {
     public String getHistoryRecordForPerson(Person person) {
         return "This is historic data of a person = " + person.getName();
     }
+
+    @Identifier(ageRestriction = 5, error = "Person is under ageRestriction protection")
+    public String get2ndHistoryRecordForPerson(Person person) {
+        return "This is 2nd degree historic data of a person = " + person.getName();
+    }
+    @Identifier(ageRestriction = 2, error = "Person is under ageRestriction protection")
+    public String get3rdHistoryRecordForPerson(Person person) {
+        return "This is 3rd degree historic data of a person = " + person.getName();
+    }
 }
